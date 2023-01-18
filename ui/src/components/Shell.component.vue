755109dbfcc6a99d811c0866047e82a986b857cf
Marco La Rosa <m@lr.id.au>
2023-01-18 16:31:02 +1100

    refactor

--- ui/src/components/Shell.component.vue
+++ ui/src/components/Shell.component.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex flex-row relative">
         <div class="relative bg-violet-400 h-screen sidebar-width">
-            <sidebar-component class="w-full bg-stone-300 h-screen" v-show="data.expanded" />
+            <sidebar-component class="w-full bg-stone-300 h-screen" />
         </div>
         <div class="w-full relative">
-            <router-view />
+            <router-view class="p-2" />
         </div>
     </div>
 </template>
@@ -12,31 +12,29 @@
 <script setup>
 import { tokenSessionKey, getLocalStorage } from "./storage.js";
 import SidebarComponent from "./Sidebar.component.vue";
-import { reactive, onMounted, inject, computed } from "vue";
+import { onMounted, inject, computed } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { useStore } from "vuex";
-import { round } from "lodash";
 const $http = inject("$http");
 const $route = useRoute();
 const $router = useRouter();
 const $store = useStore();
-const data = reactive({
-    expanded: true,
-});
+
 onMounted(() => {
     init();
 });
 
 async function init() {
     let response = await $http.get({ route: "/authenticated" });
-    if (response.status === 200) {
-        let { token } = getLocalStorage({ key: tokenSessionKey });
-        let user = JSON.parse(atob(token.split(".")[1]));
-        $store.commit("setUserData", user);
-        if ($route.path === "/") $router.push("/dashboard");
-    } else {
+    if (response.status !== 200) {
         $router.push("/login");
     }
+    let { token } = getLocalStorage({ key: tokenSessionKey });
+    let user = JSON.parse(atob(token.split(".")[1]));
+    $store.commit("setUserData", user);
+    if ($route.path === "/") $router.push("/dashboard");
+
+    $store.dispatch("getMyCollections");
 }
 </script>
 
