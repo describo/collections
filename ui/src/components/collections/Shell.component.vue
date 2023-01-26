b67df20bd3da7b51daf7314f78e531b93b6e96cb
Marco La Rosa <m@lr.id.au>
2023-01-27 10:54:51 +1100

    implement entity loading and display

--- ui/src/components/collections/Shell.component.vue
+++ ui/src/components/collections/Shell.component.vue
@@ -1,22 +1,59 @@
 <template>
-    <div class="flex flex-col">
-        <div>{{ data.code }} Collection</div>
+    <div class="flex flex-col space-y-4">
+        <div class="flex flex-row space-x-4 bg-blue-200 p-6">
+            <div class="pt-1">{{ code }}</div>
 
-        <div><el-button @click="loadDataset">load dataset</el-button></div>
+            <div class="flex-grow">
+                <el-autocomplete
+                    class="w-full"
+                    v-model="data.queryString"
+                    :fetch-suggestions="lookupEntity"
+                    clearable
+                    placeholder="Search for an entity to work with"
+                    @select="loadEntity"
+                >
+                    <template #default="{ item }"> {{ item.eid }}: {{ item.name }} </template>
+                </el-autocomplete>
+            </div>
+        </div>
+    </div>
+    <div class="overflow-scroll" :class="panelHeight">
+        <pre>{{ data.entity }}</pre>
     </div>
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, inject, computed } from "vue";
 import { useRoute } from "vue-router";
 import { useStore } from "vuex";
 const $route = useRoute();
 const $store = useStore();
+const $http = inject("$http");
 
 const data = reactive({
-    ...$route.params,
+    queryString: "",
+    entity: {},
 });
-function loadDataset() {
-    $store.dispatch("loadCollectionData", { code: $route.params.code });
+let code = computed(() => $route.params.code);
+let panelHeight = computed(() => ({ "max-height": `${window.innerHeight - 50}px` }));
+
+async function lookupEntity(query, cb) {
+    if (query.length < 4) return cb([]);
+    let response = await $http.get({
+        route: `/collections/${$route.params.code}/entities`,
+        params: { queryString: query },
+    });
+    if (response.status !== 200) return cb([]);
+    response = await response.json();
+    return cb(response.matches);
+}
+
+async function loadEntity(entity) {
+    let response = await $http.get({
+        route: `/collections/${$route.params.code}/entities/${entity.id}`,
+    });
+    if (response.status !== 200) return;
+    response = await response.json();
+    data.entity = response.entity;
 }
 </script>
