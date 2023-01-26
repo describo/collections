08a2bd5a64e4f29130fc136fe79907cc5baf6963
Marco La Rosa <m@lr.id.au>
2023-01-26 14:41:17 +1100

    add commonjs plugin

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,5 @@ export default defineConfig({
         host: "0.0.0.0",
         port: "9000",
     },
-    plugins: [vue()],
+    plugins: [vue(), viteCommonjs()],
 });
