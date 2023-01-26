31f7819fd72599c635e094f58e8ae9538f067f51
Marco La Rosa <m@lr.id.au>
2023-01-27 10:53:26 +1100

    refactor load collection; implement load entity

--- api/src/routes/collection.js
+++ api/src/routes/collection.js
@@ -3,11 +3,12 @@ import fsExtraPkg from "fs-extra";
 const { pathExists, readJSON } = fsExtraPkg;
 import path from "path";
 import { ROCrate } from "ro-crate";
+import { Op } from "sequelize";
 import models from "../models/index.js";
 import { v4 as uuidv4 } from "uuid";
 import { init } from "@paralleldrive/cuid2";
 import lodashPkg from "lodash";
-const { isPlainObject, isString, chunk } = lodashPkg;
+const { isArray, isPlainObject, isString, chunk } = lodashPkg;
 
 const createId = init({ length: 32 });
 
@@ -19,6 +20,8 @@ export function setupRoutes(fastify, options, done) {
     fastify.register((fastify, options, done) => {
         fastify.addHook("preHandler", requireCollectionAccess);
         fastify.get("/collections/:code/load", getCollectionsLoadHandler);
+        fastify.get("/collections/:code/entities", lookupEntities);
+        fastify.get("/collections/:code/entities/:entityId", loadEntity);
         done();
     });
     done();
@@ -50,121 +53,48 @@ async function getCollectionsLoadHandler(req, res) {
     const collectionsCode = req.params.code;
     const dataPath = "/srv/data";
     const crateFile = path.join(dataPath, collectionsCode, "ro-crate-metadata.json");
-    if (!(await pathExists(crateFile))) return;
 
-    // TODO: disabled for now
-    let crate = await readJSON(crateFile);
-    // console.log(JSON.stringify(crate, null, 2));
-    try {
-        console.time();
-        console.log("ingesting into rocrate js");
-        crate = new ROCrate(crate, { array: true });
-        console.log("ingesting into rocrate js");
-        console.timeEnd();
-    } catch (error) {
-        return res.badRequestError();
-    }
-
-    // let crate = {
-    //     "@context": [
-    //         "https://w3id.org/ro/crate/1.1/context",
-    //         {
-    //             "@base": null,
-    //         },
-    //     ],
-    //     "@graph": [
-    //         {
-    //             "@id": "ro-crate-metadata.json",
-    //             "@type": "CreativeWork",
-    //             conformsTo: {
-    //                 "@id": "https://w3id.org/ro/crate/1.1",
-    //             },
-    //             about: {
-    //                 "@id": "./",
-    //             },
-    //         },
-    //         {
-    //             "@id": "./",
-    //             "@type": "Dataset",
-    //             "@reverse": {},
-    //             name: "My Research Object Crate",
-    //             person: [
-    //                 {
-    //                     "@id": "P1",
-    //                 },
-    //             ],
-    //         },
-    //         {
-    //             "@id": "P1",
-    //             "@type": "Entity",
-    //             entityType: [{ "@id": "ET1" }, { "@id": "ET2" }],
-    //             ohrmID: { "@id": "O1" },
-    //             name: "Person 1",
-    //             something: "value",
-    //         },
-    //         { "@id": "ET1", "@type": "EntityType", name: "Person" },
-    //         { "@id": "ET2", "@type": "EntityType", name: "School" },
-    //         { "@id": "O1", "@type": "OHRMID", name: "P1" },
-    //         {
-    //             "@id": "S1",
-    //             "@type": "Entity",
-    //             entityType: [{ "@id": "ET3" }],
-    //             name: "School 1",
-    //         },
-    //         { "@id": "ET3", "@type": "EntityType", name: "School" },
-    //         { "@id": "R1", "@type": "EntityType", entityType: { "@id": "ET4" }, name: "P1 => S1" },
-    //         {
-    //             "@id": "ET4",
-    //             "@type": "Relationship",
-    //             name: "PS => S1 relationship",
-    //             relationshipSubject: { "@id": "P1" },
-    //             relationshipObject: { "@id": "S1" },
-    //         },
-    //     ],
-    // };
+    // return if collection already has entities
     const collectionId = req.session.collection.id;
-    console.log("***", req.session.collection.get());
+    let entityCount = await models.entity.count({
+        where: { collectionId },
+    });
+    if (entityCount !== 0) return {};
+
+    // return if no crate file found
+    if (!(await pathExists(crateFile))) return {};
+
+    // load the crate file
+    let crate = await readJSON(crateFile);
 
     const entitiesByDescriboId = {};
     const entitiesByAtId = {};
-    // console.log(crate.toJSON());
-    crate.rootDataset.label = "rootDataset";
+    const rootDescriptor = crate["@graph"].filter((e) => e["@id"] === "ro-crate-metadata.json")[0];
     console.log(`Assigning identifiers`, new Date());
-    for (let entity of crate.entities()) {
+    for (let entity of crate["@graph"]) {
+        if (entity["@id"] === rootDescriptor.about["@id"]) entity.label = "RootDescriptor";
         entity.describoId = createId();
-        entitiesByDescriboId[entity.describoId] = entity.toJSON();
-        entitiesByAtId[entity["@id"]] = entity.toJSON();
-        // console.log(entity.toJSON());
+        entitiesByDescriboId[entity.describoId] = entity;
+        entitiesByAtId[entity["@id"]] = entity;
     }
-    // console.log(entitiesByAtId);
-
-    // console.log(entitiesByAtId);
-    // const crate = {
-    //     '@context': '',
-    //     '@graph',
-    // }
-    // console.log(req.session.collection);
-    // for (let entity of crate.entities()) {
-    //     entity.describoId = uuidv4();
-    // }
-    // for (let entity of crate.entities()) {
-    //     console.log(entity.toJSON());
-    // }
 
     console.time();
     console.log(`Preparing the inserts`, new Date());
     const entityMainProperties = ["@id", "@type", "name", "label", "describoId"];
     let entityInserts = [];
     let propertyInserts = [];
-    for (let entity of crate.entities()) {
+    for (let entity of crate["@graph"]) {
         entityInserts.push({
-            id: entity.describoId[0],
+            id: entity.describoId,
             eid: entity["@id"],
             etype: JSON.stringify(entity["@type"]),
-            name: entity?.name?.[0],
+            name: entity?.name,
             collectionId,
         });
         for (let property of Object.keys(entity)) {
+            if (!isArray(entity[property]) && property !== "describoId") {
+                entity[property] = [entity[property]];
+            }
             if (!entity[property]) continue;
             for (let instance of entity[property]) {
                 if (!entityMainProperties.includes(property)) {
@@ -178,7 +108,7 @@ async function getCollectionsLoadHandler(req, res) {
                         propertyInserts.push({
                             id: createId(),
                             property,
-                            entityId: entity.describoId[0],
+                            entityId: entity.describoId,
                             value: instance,
                             collectionId,
                         });
@@ -193,7 +123,7 @@ async function getCollectionsLoadHandler(req, res) {
                             propertyInserts.push({
                                 id: createId(),
                                 property,
-                                entityId: entity.describoId[0],
+                                entityId: entity.describoId,
                                 targetEntityId: targetEntity.describoId,
                                 collectionId,
                             });
@@ -201,7 +131,7 @@ async function getCollectionsLoadHandler(req, res) {
                             propertyInserts.push({
                                 id: createId(),
                                 property,
-                                entityId: entity.describoId[0],
+                                entityId: entity.describoId,
                                 valueStringified: JSON.stringify(instance),
                                 collectionId,
                             });
@@ -215,9 +145,9 @@ async function getCollectionsLoadHandler(req, res) {
     console.log("inserting db records");
     let chunkSize = 100;
     if (entityInserts.length < 10000) {
-        chunkSize = 1000;
+        chunkSize = 5000;
     } else if (entityInserts.length < 100000) {
-        chunkSize = 10000;
+        chunkSize = 20000;
     } else if (entityInserts.length < 1000000) {
         chunkSize = 50000;
     } else {
@@ -231,9 +161,9 @@ async function getCollectionsLoadHandler(req, res) {
 
     chunkSize = 100;
     if (propertyInserts.length < 10000) {
-        chunkSize = 1000;
+        chunkSize = 5000;
     } else if (propertyInserts.length < 100000) {
-        chunkSize = 10000;
+        chunkSize = 20000;
     } else if (propertyInserts.length < 1000000) {
         chunkSize = 50000;
     } else {
@@ -252,4 +182,61 @@ async function getCollectionsLoadHandler(req, res) {
     // walk the graph rewriting all @id ref's to point to describoId
     // ingest the graph
     return {};
+}
+
+async function lookupEntities(req) {
+    // console.log(req.session.collection);
+    const queryString = req.query.queryString;
+    let matches = await models.entity.findAll({
+        where: {
+            collectionId: req.session.collection.id,
+            [Op.or]: [
+                {
+                    eid: {
+                        [Op.iLike]: `${queryString}%`,
+                    },
+                },
+                {
+                    name: {
+                        [Op.iLike]: `${queryString}%`,
+                    },
+                },
+            ],
+        },
+        limit: 10,
+    });
+
+    return { matches: matches.map((m) => ({ ...m.get(), etype: JSON.parse(m.etype) })) };
+}
+
+async function loadEntity(req) {
+    let entity = await models.entity.findOne({
+        where: { id: req.params.entityId, collectionId: req.session.collection.id },
+        include: [
+            {
+                model: models.property,
+                include: [{ model: models.entity, as: "targetEntity" }],
+            },
+        ],
+    });
+    let properties = {};
+    for (let p of entity.properties) properties[p.property] = [];
+    for (let p of entity.properties) {
+        if (p.value) properties[p.property].push({ id: p.id, value: p.value });
+        if (p.valueStringified)
+            properties[p.property].push({ id: p.id, value: JSON.parse(p.value) });
+        if (p.targetEntity)
+            properties[p.property].push({
+                ...p.targetEntity.get(),
+                etype: JSON.parse(p.targetEntity.etype),
+            });
+    }
+    let entityData = entity.get();
+    delete entityData.properties;
+    entityData = {
+        ...entityData,
+        etype: JSON.parse(entity.etype),
+        ...properties,
+    };
+    return { entity: entityData };
 }
