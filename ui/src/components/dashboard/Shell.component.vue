b5d63d17b11d9006990d7254db1f0adff8989411
Marco La Rosa <m@lr.id.au>
2023-01-27 13:52:21 +1100

    placeholder text

--- ui/src/components/dashboard/Shell.component.vue
+++ ui/src/components/dashboard/Shell.component.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>intentionally blank for now</div>
+    <div class="flex flex-col p-4 space-y-2">
+        <div class="text-2xl text-gray-800 tracking-wider border-b border-solid py-2">
+            Dashboard
+        </div>
+        <div>in progress</div>
+    </div>
 </template>
 
 <script></script>
