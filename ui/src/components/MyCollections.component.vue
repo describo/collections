f3be6963b7507b792026bfe3fc806fa011da7942
Marco La Rosa <m@lr.id.au>
2023-01-27 13:52:29 +1100

    colour active ohrm

--- ui/src/components/MyCollections.component.vue
+++ ui/src/components/MyCollections.component.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="flex flex-col">
         <div v-for="collection of collections" :key="collection.id">
-            <router-link :to="collectionLink(collection.code)" class="text-xl text-gray-800">
+            <router-link
+                :to="collectionLink(collection.code)"
+                class="text-xl text-gray-800"
+                :class="{ 'text-blue-600': activeCollection === collection.code }"
+            >
                 {{ collection.code }}
             </router-link>
         </div>
@@ -10,10 +14,13 @@
 
 <script setup>
 import { computed } from "vue";
+import { useRoute } from "vue-router";
 import { useStore } from "vuex";
 const $store = useStore();
+const $route = useRoute();
 
 let collections = computed(() => $store.state.myCollections);
+let activeCollection = computed(() => $route.params.code);
 
 function collectionLink(code) {
     return `/collections/${code}`;
