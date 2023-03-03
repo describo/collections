ed2861be6825ad7b45662ca53477139c65ee8895
Marco La Rosa <m@lr.id.au>
2023-03-03 14:30:46 +1100

    revise to create entities with type associations

--- api/src/routes/collection.js
+++ api/src/routes/collection.js
@@ -8,8 +8,7 @@ import models from "../models/index.js";
 import { v4 as uuidv4 } from "uuid";
 import { init } from "@paralleldrive/cuid2";
 import lodashPkg from "lodash";
-const { isArray, isPlainObject, isString, chunk } = lodashPkg;
-
+const { isArray, isPlainObject, isString, chunk, groupBy } = lodashPkg;
 const createId = init({ length: 32 });
 
 export function setupRoutes(fastify, options, done) {
@@ -67,12 +66,23 @@ async function collectionLoadHandler(req, res) {
     const dataPath = "/srv/data";
     const crateFile = path.join(dataPath, collectionsCode, "ro-crate-metadata.json");
 
-    // return if collection already has entities
     const collectionId = req.session.collection.id;
-    let entityCount = await models.entity.count({
-        where: { collectionId },
-    });
-    if (entityCount !== 0) return {};
+
+    // delete the collection data if there's already some loaded and we're in development
+    if (process.env.NODE_ENV === "development") {
+        req.io
+            .to(req.query.clientId)
+            .emit("load-collection-data", { msg: `Deleting existing data`, date: new Date() });
+        await this.models.type.destroy({ where: { collectionId } });
+        await this.models.property.destroy({ where: { collectionId } });
+        await this.models.entity.destroy({ where: { collectionId } });
+    } else {
+        // complain loudly and bail if this collection already has data
+        let entityCount = await models.entity.count({
+            where: { collectionId },
+        });
+        if (entityCount !== 0) return {};
+    }
 
     // return if no crate file found
     if (!(await pathExists(crateFile))) return {};
@@ -94,14 +104,27 @@ async function collectionLoadHandler(req, res) {
     req.io
         .to(req.query.clientId)
         .emit("load-collection-data", { msg: `Assigning entity identifiers`, date: new Date() });
+
+    let types = {};
     for (let entity of crate["@graph"]) {
         if (entity["@id"] === "ro-crate-metadata.json") entity.label = "RootDescriptor";
         if (entity["@id"] === rootDescriptor.about["@id"]) entity.label = "RootDataset";
         entity.describoId = createId();
         entity["@type"] = asArray(entity["@type"]);
+        entity["@type"].forEach((type) => (types[type] = true));
         entitiesByDescriboId[entity.describoId] = entity;
         entitiesByAtId[entity["@id"]] = entity;
     }
+
+    const typeInserts = Object.keys(types).map((type) => {
+        return {
+            id: createId(),
+            name: type,
+            collectionId,
+        };
+    });
+    const typeNameToIdMapping = groupBy(typeInserts, "name");
+    await this.models.type.bulkCreate(typeInserts);
 
     // prepare the entity and property DB inserts
     req.io.to(req.query.clientId).emit("load-collection-data", {
@@ -112,15 +135,23 @@ async function collectionLoadHandler(req, res) {
     const entityMainProperties = ["@id", "@type", "name", "label", "describoId"];
     let entityInserts = [];
     let propertyInserts = [];
+    let entityTypeInserts = [];
     for (let entity of crate["@graph"]) {
         entityInserts.push({
             id: entity.describoId,
             eid: entity["@id"],
-            etype: concat(entity["@type"]),
             name: entity?.name,
             label: entity.label,
             collectionId,
         });
+
+        entity["@type"].forEach((type) => {
+            entityTypeInserts.push({
+                entityId: entity.describoId,
+                typeId: typeNameToIdMapping[type][0].id,
+            });
+        });
+
         for (let property of Object.keys(entity)) {
             if (!isArray(entity[property]) && property !== "describoId") {
                 entity[property] = [entity[property]];
@@ -160,25 +191,21 @@ async function collectionLoadHandler(req, res) {
             }
         }
     }
+    // console.log(JSON.stringify(entityInserts, null, 2));
+    // console.log(JSON.stringify(entityTypeInserts, null, 2));
+
     await new Promise((resolve) => setTimeout(resolve, 100));
 
     // insert entities into the DB
     req.io
         .to(req.query.clientId)
         .emit("load-collection-data", { msg: `Inserting DB records`, date: new Date() });
-    let chunkSize = 100;
-    if (entityInserts.length < 10000) {
-        chunkSize = 5000;
-    } else if (entityInserts.length < 100000) {
-        chunkSize = 20000;
-    } else if (entityInserts.length < 1000000) {
-        chunkSize = 50000;
-    } else {
-        chunkSize = 50000;
-    }
+    let chunkSize = getChunkSize(entityInserts);
     for (let records of chunk(entityInserts, chunkSize)) {
         const total = records.length;
-        await models.entity.bulkCreate(records);
+        await this.models.entity.bulkCreate(records, {
+            // include: [{ model: this.models.type, as: "etype" }],
+        });
         req.io.to(req.query.clientId).emit("load-collection-data", {
             msg: `Inserted ${total} entity records`,
             date: new Date(),
@@ -186,20 +213,23 @@ async function collectionLoadHandler(req, res) {
         await new Promise((resolve) => setTimeout(resolve, 100));
     }
 
-    // insert properties into the DB
-    chunkSize = 100;
-    if (propertyInserts.length < 10000) {
-        chunkSize = 5000;
-    } else if (propertyInserts.length < 100000) {
-        chunkSize = 20000;
-    } else if (propertyInserts.length < 1000000) {
-        chunkSize = 50000;
-    } else {
-        chunkSize = 50000;
+    // create entity -> type associations
+    chunkSize = getChunkSize(entityTypeInserts);
+    for (let records of chunk(entityTypeInserts, chunkSize)) {
+        const total = records.length;
+        await this.models.entity_types.bulkCreate(records);
+        req.io.to(req.query.clientId).emit("load-collection-data", {
+            msg: `Inserted ${total} entity type associations`,
+            date: new Date(),
+        });
+        await new Promise((resolve) => setTimeout(resolve, 100));
     }
+
+    // insert properties into the DB
+    chunkSize = getChunkSize(propertyInserts);
     for (let records of chunk(propertyInserts, chunkSize)) {
         const total = records.length;
-        await models.property.bulkCreate(records);
+        await this.models.property.bulkCreate(records);
         req.io.to(req.query.clientId).emit("load-collection-data", {
             msg: `Inserted ${total} property records`,
             date: new Date(),
@@ -215,6 +245,20 @@ async function collectionLoadHandler(req, res) {
     });
 
     return {};
+
+    function getChunkSize(inserts) {
+        let chunkSize = 100;
+        if (inserts.length < 10000) {
+            chunkSize = 5000;
+        } else if (inserts.length < 100000) {
+            chunkSize = 20000;
+        } else if (inserts.length < 1000000) {
+            chunkSize = 50000;
+        } else {
+            chunkSize = 50000;
+        }
+        return chunkSize;
+    }
 }
 
 // TODO this code does not have tests
@@ -266,6 +310,10 @@ async function loadEntity(req) {
         },
         include: [
             {
+                model: models.type,
+                as: "etype",
+            },
+            {
                 model: models.property,
                 include: [{ model: models.entity, as: "targetEntity" }],
             },
@@ -306,7 +354,7 @@ async function loadEntity(req) {
     entityData = {
         describoId: entity.id,
         "@id": entity.eid,
-        "@type": entity.etype,
+        "@type": entity.etype.map((type) => type.name),
         name: entity.name,
         properties,
     };
