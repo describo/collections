3d9a25110d368b3f2854ccdd668fb0e77c4e8e55
Marco La Rosa <m@lr.id.au>
2023-03-27 14:44:06 +1100

    rework component using composition; show only configured auth providers

--- ui/src/components/Login.component.vue
+++ ui/src/components/Login.component.vue
@@ -6,48 +6,44 @@
                     class="w-1/2 text-right pr-4 text-gray-600 text-4xl flex flex-col justify-center"
                 >
                     <div>
-                        {{ siteName }}
+                        {{ data.siteName }}
                     </div>
                 </div>
                 <div class="w-1/2 pl-4 flex flex-col space-y-4">
-                    <oauth-login-component
-                        v-for="provider of loginProviders"
+                    <div
+                        v-for="provider of data.loginProviders"
                         :key="provider.name"
-                        :provider="provider.name"
-                        :button-text="provider.text"
-                        class="border-l-4 border-solid p-4 cursor-pointer hover:border-yellow-400 hover:bg-yellow-100 focus:ring-2 focus:ring-yellow-100"
-                    />
+                        class="flex-grow"
+                    >
+                        <oauth-login-component
+                            class="w-full border-l-4 border-solid p-4 cursor-pointer hover:border-yellow-400 hover:bg-yellow-100 focus:ring-2 focus:ring-yellow-100"
+                            v-if="enableProvider(provider.name)"
+                            :provider="provider.name"
+                            :button-text="provider.text"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<script>
-import { defineAsyncComponent } from "vue";
+<script setup>
+import { reactive, defineAsyncComponent, computed } from "vue";
+import { useStore } from "vuex";
+const $store = useStore();
 
-export default {
-    components: {
-        OauthLoginComponent: defineAsyncComponent(() =>
-            import("./authentication/OauthLogin.component.vue")
-        ),
-    },
-    data() {
-        return {
-            siteName: this.$store.state.configuration.ui.siteName,
-            loginProviders: [
-                { name: "aaf", text: "Login with the AAF" },
-                { name: "google", text: "Login with Google" },
-            ],
-        };
-    },
-    computed: {
-        enableGoogleLogin() {
-            return this.$store.state.configuration.authentication.includes("google");
-        },
-        enableAafLogin() {
-            return this.$store.state.configuration.authentication.includes("aaf");
-        },
-    },
-};
+const OauthLoginComponent = defineAsyncComponent(() =>
+    import("./authentication/OauthLogin.component.vue")
+);
+const data = reactive({
+    siteName: $store.state.configuration.ui.siteName,
+    loginProviders: [
+        { name: "aaf", text: "Login with the AAF" },
+        { name: "google", text: "Login with Google" },
+    ],
+});
+function enableProvider(name) {
+    return this.$store.state.configuration.authentication.includes(name);
+}
 </script>
