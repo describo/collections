8f074d61e050c945b6e24e500bd55d133b72e722
Marco La Rosa <m@lr.id.au>
2023-09-13 13:38:16 +1000

    bugfixing build script

--- build-production-containers.sh
+++ build-production-containers.sh
@@ -4,18 +4,17 @@
 
 VERSION=$(grep version api/package.json | awk -F ': ' '{ print $2 }' | sed 's/"//g' | sed 's/,//' )
 
-whereis docker
-
 docker build --push --rm \
     -t ghcr.io/describo/describo-collections-api:latest \
     -t ghcr.io/describo/describo-collections-api:${VERSION} \
     -f Dockerfile.api-build .
 
-docker run -it --rm \
-    -v $PWD/ui:/srv/ui \
-    -v ui_node_modules:/srv/ui/node_modules \
-    -v $PWD/../crate-builder-component:/srv/describo \
-    -w /srv/ui node:14-buster bash -l -c "npm run build"
+npm run build
+# docker run -it --rm \
+#     -v $PWD/ui:/srv/ui \
+#     -v ui_node_modules:/srv/ui/node_modules \
+#     -v $PWD/../crate-builder-component:/srv/describo \
+#     -w /srv/ui node:14-buster bash -l -c "npm run build"
 docker build --push --rm \
     -t ghcr.io/describo/describo-collections-ui:latest \
     -t ghcr.io/describo/describo-collections-ui:${VERSION} \
