89663709f33b021f3aab936cc9cc983447c2cf5d
Marco La Rosa <m@lr.id.au>
2023-09-13 13:19:52 +1000

    re-enable build commands in build script

--- build-production-containers.sh
+++ build-production-containers.sh
@@ -1,16 +1,18 @@
 # #!/usr/bin/env bash
 
-#     docker build --push --rm \
-#         -t ghcr.io/describo/describo-collections-api:latest \
-#         -t ghcr.io/describo/describo-collections-api:${VERSION} \
-#         -f Dockerfile.api-build .
+apt-get update && apt-get install -y docker
 
-#     docker run -it --rm \
-#         -v $PWD/ui:/srv/ui \
-#         -v ui_node_modules:/srv/ui/node_modules \
-#         -v $PWD/../crate-builder-component:/srv/describo \
-#         -w /srv/ui node:14-buster bash -l -c "npm run build"
-#     docker build --push --rm \
-#         -t ghcr.io/describo/describo-collections-ui:latest \
-#         -t ghcr.io/describo/describo-collections-ui:${VERSION} \
-#         -f Dockerfile.ui-build .
+docker build --push --rm \
+    -t ghcr.io/describo/describo-collections-api:latest \
+    -t ghcr.io/describo/describo-collections-api:${VERSION} \
+    -f Dockerfile.api-build .
+
+docker run -it --rm \
+    -v $PWD/ui:/srv/ui \
+    -v ui_node_modules:/srv/ui/node_modules \
+    -v $PWD/../crate-builder-component:/srv/describo \
+    -w /srv/ui node:14-buster bash -l -c "npm run build"
+docker build --push --rm \
+    -t ghcr.io/describo/describo-collections-ui:latest \
+    -t ghcr.io/describo/describo-collections-ui:${VERSION} \
+    -f Dockerfile.ui-build .
